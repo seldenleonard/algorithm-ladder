ac072bf225c095c94968e94112f7fcdd016255fc
seldenleonard <selden222@gmail.com>
2022-06-15 03:50:57 -0700

    solves reduce sum problem

--- basic-array-problems/reduce-sum.rb
+++ basic-array-problems/reduce-sum.rb
@@ -10,6 +10,7 @@ def reduce_sum(ar)
   while i < ar.length
     sum = sum + ar[i]
     i += 1
+    # sum = sum + ar[i] <---> I cant figure out why placing this line of code here instead of two lines above does not work, even when I change the original index value to -1
   end
   sum
 end
