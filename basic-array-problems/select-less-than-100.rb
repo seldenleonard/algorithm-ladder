8c9fa9ae48f0d2a9135c63c45c82a211d5cac83a
seldenleonard <selden222@gmail.com>
2022-06-15 04:25:01 -0700

    completes first two whiteboarding steps

--- basic-array-problems/select-less-than-100.rb
+++ basic-array-problems/select-less-than-100.rb
@@ -1,5 +1,16 @@
 # Clarify
+  # Input: [99, 101, 88, 4, 2000, 50]
+  # Output: [99, 88, 4, 50]
 # Visualize
+  # For each value in the array, determine if it IS or IS NOT below 100
+  # If a given value IS less than 100, add it to the array that will be output
+  # Variables that must be defined: array of integers less than 100
 # Write the Code
 # Debugger
 # Refactor
+
+def select_less_than_100(ar)
+ 
+end
+
+p select_less_than_100([99, 101, 88, 4, 2000, 50])
