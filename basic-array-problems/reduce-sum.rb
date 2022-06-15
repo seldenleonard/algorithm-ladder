9017535a2eee037c1ef29d8b6d78d15e8f132eaf
seldenleonard <selden222@gmail.com>
2022-06-15 03:54:22 -0700

    refactors reduce sum problem

--- basic-array-problems/reduce-sum.rb
+++ basic-array-problems/reduce-sum.rb
@@ -8,7 +8,7 @@ def reduce_sum(ar)
   i = 0
   sum = 0
   while i < ar.length
-    sum = sum + ar[i]
+    sum += + ar[i]
     i += 1
     # sum = sum + ar[i] <---> I cant figure out why placing this line of code here instead of two lines above does not work, even when I change the original index value to -1
   end
