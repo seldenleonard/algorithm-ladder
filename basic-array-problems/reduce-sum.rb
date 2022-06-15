93f7955a22c42653abb2e97c35ced74a4a723fbd
seldenleonard <selden222@gmail.com>
2022-06-15 03:23:36 -0700

    writes first attempt at reduce sum problem

--- basic-array-problems/reduce-sum.rb
+++ basic-array-problems/reduce-sum.rb
@@ -1,5 +1,14 @@
 # Clarify
-# Visualizr
+# Visualize
 # Write the Code
 # Debugger
 # Refactor
+
+def reduce_sum(ar)
+  i = 0
+  sum = 0
+  while i < ar.length
+    sum = sum + ar(i)
+  end
+  sum
+end
