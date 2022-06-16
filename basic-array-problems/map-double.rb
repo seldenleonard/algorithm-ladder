35626b7ee9443f247321d5c2271dca1be5342733
seldenleonard <selden222@gmail.com>
2022-06-15 22:41:07 -0700

    adds instructions

---
+++ basic-array-problems/map-double.rb
@@ -0,0 +1,12 @@
+# INSTRUCTIONS
+# Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+  # Input: [4, 2, 5, 99, -4]
+  # Output: [8, 4, 10, 198, -8]
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
