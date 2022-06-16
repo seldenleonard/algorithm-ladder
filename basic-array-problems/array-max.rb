8457031b3cbf137ebabbe9635350e21bcf7fe5e8
seldenleonard <selden222@gmail.com>
2022-06-15 23:17:12 -0700

    solves array-max algorithm

--- basic-array-problems/array-max.rb
+++ basic-array-problems/array-max.rb
@@ -14,3 +14,16 @@
 # Define a variable called greatest_value. Set it to the first value of the input array.
 # Iterate through the array and compare each value to the greatest_value, setting greatest_value to whichever value is greater.
 
+def array_max(ar)
+  i = 1
+  greatest_value = ar[0]
+  while i < ar.length
+    if ar[i] > greatest_value
+      greatest_value = ar[i]
+    end
+    i +=1
+  end
+  greatest_value
+end
+
+p array_max([5, 17, -4, 20, 12])
