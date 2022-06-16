d3f2d39129efb46fce546f6f6f7f00876ddab590
seldenleonard <selden222@gmail.com>
2022-06-15 22:58:04 -0700

    adds instructions

---
+++ basic-array-problems/array-max.rb
@@ -0,0 +1,11 @@
+# INSTRUCTIONS
+# Write a function that returns the greatest value from an array of numbers.
+  # Input: [5, 17, -4, 20, 12]
+  # Output: 20
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
