cee10a6347361922ed04872ce1f01923a74b308e
seldenleonard <selden222@gmail.com>
2022-06-16 22:12:30 -0700

    writes out whiteboarding steps

--- basic-string-problems/reverse-string.rb
+++ basic-string-problems/reverse-string.rb
@@ -1,4 +1,18 @@
+# INSTRUCTIONS
 # Write a function that returns the reverse of a given string.
   # Input: “abcde”
   # Output: “edcba”
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
+# Define a method that accepts a string
+# Convert string to an array
+# Starting at the last value in the array, iterate backwards, and insert each value into a new array
+# Convert new array to string
+# Output the string
 
