4d810d0046206c2c19ccfd1cbf90d3f4137ff60f
seldenleonard <selden222@gmail.com>
2022-06-17 01:02:13 -0700

    creates show-me-the-money.rb file and adds insturctions

---
+++ basic-string-problems/show-me-the-money.rb
@@ -0,0 +1,12 @@
+# INSTRUCTIONS
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+  # Input: “i hate $ but i love money i know i know im crazy”
+  # Output: true
+  # Input: “abcdefghijklmnopqrstuvwxyz”
+  # Output: false
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
