62dab44ec915a5e7b4d30cf06e3ff2ff2259fed9
seldenleonard <selden222@gmail.com>
2022-06-17 02:49:09 -0700

    adds instructions and whitebaording steps for alt solution to money algorithm

---
+++ basic-string-problems/show-me-the-money-alternate-solution.rb
@@ -0,0 +1,19 @@
+# INSTRUCTIONS
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+  # Input: “i hate $ but i love money i know i know im crazy”
+  # Output: true
+  # Input: “abcdefghijklmnopqrstuvwxyz”
+  # Output: false
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
+# Define a method that accepts a string
+# Iterate through values in the array until reaching a "$", or until all values have been evaluated
+# Use an if-statement to determine whether each value IS or IS NOT a "$"
+# If a "$" is present, return true, otherwise return false.
+
