7d1f7cd8c87b3a63f7b358bc7126804d34894f23
seldenleonard <selden222@gmail.com>
2022-06-17 03:43:45 -0700

    adds instructions

---
+++ basic-string-problems/first-duplicate-character.rb
@@ -0,0 +1,4 @@
+# INSTRUCTIONS
+# Given a string, write a function that returns the first occurence of two duplicate characters in a row, and return the duplicated character.
+  # Input: “abcdefghhijkkloooop”
+  # Output: “h”
