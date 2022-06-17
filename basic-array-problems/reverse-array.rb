b7751380ce0bbcd8f89793109b8934ad6e1baf0f
seldenleonard <selden222@gmail.com>
2022-06-16 17:24:12 -0700

    writes whiteboarding steps for reverse-array.rb

--- basic-array-problems/reverse-array.rb
+++ basic-array-problems/reverse-array.rb
@@ -3,3 +3,18 @@
   # Input: [1, 2, 3, 4, 5]
   # Output: [5, 4, 3, 2, 1]
 
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
+# Define a method that accepts an array
+# Set a variable called "rev" to equal an empty array
+# Starting at the last value in the array, iterate backwards, and at each value, enter that value into the "rev" array
+# Output "rev"
+
+def reverse
+  
+end
