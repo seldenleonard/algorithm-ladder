c73936b1d9059b6c8a2f7c1864622fe2ebe2c797
seldenleonard <selden222@gmail.com>
2022-06-16 22:07:43 -0700

    adds insturctions

--- basic-array-problems/reduce-sum.rb
+++ basic-array-problems/reduce-sum.rb
@@ -1,3 +1,10 @@
+# INSTRUCTIONS
+# Write a function that returns the sum of all numbers in a given array.
+# NOTE: Do not use any built-in language functions that do this automatically for you.
+  # Input: [1, 2, 3, 4]
+  # Output: 10
+# Explanation: (1 + 2 + 3 + 4) = 10
+
 # Clarify
 # Visualize
 # Write the Code
