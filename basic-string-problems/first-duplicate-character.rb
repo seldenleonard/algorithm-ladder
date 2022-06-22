7f58e672ce9c16196b483b930c080bfff2177ee5
seldenleonard <selden222@gmail.com>
2022-06-21 23:46:52 -0700

    creates palindrome.rb file

--- basic-string-problems/first-duplicate-character.rb
+++ basic-string-problems/first-duplicate-character.rb
@@ -2,3 +2,4 @@
 # Given a string, write a function that returns the first occurence of two duplicate characters in a row, and return the duplicated character.
   # Input: “abcdefghhijkkloooop”
   # Output: “h”
+
