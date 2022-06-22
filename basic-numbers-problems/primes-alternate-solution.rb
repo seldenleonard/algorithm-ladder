a950b9775507c852b4d864a355eb83a0702231d3
seldenleonard <selden222@gmail.com>
2022-06-22 16:53:07 -0700

    creates alternate solution file for primes algorithm, refactors solution, and solves it using half the steps

--- basic-numbers-problems/primes-alternate-solution.rb
+++ basic-numbers-problems/primes-alternate-solution.rb
@@ -11,14 +11,18 @@
 # Define a method that accepts an integer
 # If that integer is greater than 1 and only divisible by itself and 1, return true. If that number is divisible by a number greater than 1 and less than itself, return false
 
+# REFACTORING IDEAS
+# No even number is prime, so only need algorithm to evaluate odd numbers
+# In order to refactor, have the iterations stop like halfway, because for example no number is going to be divisible by a number greater than half of itself. like 100 cannot be divisble by 51. Im sure there an even smarter way to do this and save steps ending somewehre before halfway as well
+
 def prime?(int)
-  i = 2
+  i = 3
   is_prime = true
   while i < int
     if int % i == 0
       is_prime = false
     end
-    i += 1
+    i += 2
   end
   is_prime
 end
