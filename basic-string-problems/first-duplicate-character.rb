ddf184d07a74376a57c3b7bae728a0f7e0d1be09
seldenleonard <selden222@gmail.com>
2022-06-22 02:06:40 -0700

    writes steps for solving first duplicate problem

--- basic-string-problems/first-duplicate-character.rb
+++ basic-string-problems/first-duplicate-character.rb
@@ -10,7 +10,9 @@
 # Refactor
 
 # STEPS
-# 
+# Define a method that accepts a string
+# Define 2 index variables
+# Iterate through the string using the 2 indexes, and compare the values to determine the first occurence of two duplicate characters in a row
 
 def first_duplicate(str)
   
