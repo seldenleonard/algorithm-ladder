ad581e1c8edddf483c74087340c216ab363746e4
seldenleonard <selden222@gmail.com>
2022-06-22 03:35:28 -0700

    solves first duplicate character algorithm and revises steps

--- basic-string-problems/first-duplicate-character.rb
+++ basic-string-problems/first-duplicate-character.rb
@@ -11,9 +11,18 @@
 
 # STEPS
 # Define a method that accepts a string
-# Define 2 index variables
-# Iterate through the string using the 2 indexes, and compare the values to determine the first occurence of two duplicate characters in a row
+# Define an index variable
+# Iterate through the string using the index and the index - 1, and compare the values to determine the first occurence of two duplicate characters in a row
 
 def first_duplicate(str)
-  
+  i = 1
+  while i < str.length
+    if str[i] == str[i - 1]
+      return str[i]
+    end
+    i += 1
+  end
+  nil
 end
+
+p first_duplicate("abcde")
