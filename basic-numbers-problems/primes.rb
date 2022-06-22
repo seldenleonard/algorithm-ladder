d887cfd8c1549d5b7422fb983dbed226dafcef12
seldenleonard <selden222@gmail.com>
2022-06-22 15:08:46 -0700

    writes first attempt to solve primes.rb

--- basic-numbers-problems/primes.rb
+++ basic-numbers-problems/primes.rb
@@ -11,3 +11,14 @@
 # Define a method that accepts an integer
 # If that integer is greater than 1 and only divisible by itself and 1, return true. If that number is divisible by a number greater than 1 and less than itself, return false
 
+# In order to refactor, have the iterations stop like halfway, because for example no number is going to be divisible by a number greater than half of itself. like 100 cannot be divisble by 51. Im sure there an even smarter way to do this and save steps ending somewehre before halfway as well
+
+def prime?(int)
+  n = 2
+  while n < int
+    true if int % n == 0
+    n += 1
+  end
+end
+
+p prime?(9)
