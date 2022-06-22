26292a0a185eaa0d428a55feaaf86155a9b4391d
seldenleonard <selden222@gmail.com>
2022-06-21 23:47:17 -0700

    adds instructions

---
+++ basic-string-problems/palindrome.rb
@@ -0,0 +1,7 @@
+Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+
+Input: “racecar”
+Output: true
+
+Input: “baloney”
+Output: false
