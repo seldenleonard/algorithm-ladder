2e514d2a3eefef5b3fadc793a012fc3f1a34ac3b
seldenleonard <selden222@gmail.com>
2022-06-22 14:24:36 -0700

    creates file for and solves rever-words-alternate-solution.rb

---
+++ basic-string-problems/reverse-words-alternate-solution.rb
@@ -0,0 +1,20 @@
+# INSTRUCTIONS
+# Given a string of words, write a function that returns a new string that contains the words in reverse order.
+  # Input: “popcorn is so cool isn’t it yeah i thought so”
+  # Output: “so thought i yeah it isn’t cool so is popcorn”
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
+# Define a method that accepts a string
+# Create an empty string
+
+def reverse_words(str)
+  str.split.reverse.join(" ")
+end
+
+reverse_words("popcorn is so cool isn't it yeah i thought so")
