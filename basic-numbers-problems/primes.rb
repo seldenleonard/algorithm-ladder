13f2c61e30f7c4ee27de61dde246c98169d47983
seldenleonard <selden222@gmail.com>
2022-06-22 14:54:08 -0700

    writes whiteboarding steps to complete prime.rb algorithm

--- basic-numbers-problems/primes.rb
+++ basic-numbers-problems/primes.rb
@@ -1,2 +1,13 @@
 # INSTRUCTIONS
 # Write a function that returns whether a given number is a prime number.
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
+# Define a method that accepts an integer
+# If that integer is greater than 1 and only divisible by itself and 1, return true. If that number is divisible by a number greater than 1 and less than itself, return false
+
