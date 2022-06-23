cf12af60ec82ccaa15b55ec4cca634ffd38fc8b6
seldenleonard <selden222@gmail.com>
2022-06-23 01:09:15 -0700

    adds fizzbuzz instructions

---
+++ basic-numbers-problems/fizzbuzz.rb
@@ -0,0 +1,13 @@
+# INSTRUCTIONS
+# Write a function that prints out every number from 1 to N, with the following exceptions:
+  # If the number is divisible by 3, print out "FIZZ".
+  # If the number is divisible by 5, print out "BUZZ".
+  # If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
