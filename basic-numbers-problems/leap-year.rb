8589d65c68f22dfaa57c5043d9f98e67551c0147
seldenleonard <selden222@gmail.com>
2022-06-23 04:26:28 -0700

    successfully refactors leap year solution

--- basic-numbers-problems/leap-year.rb
+++ basic-numbers-problems/leap-year.rb
@@ -21,35 +21,37 @@
   # If year is divisible by 100, leap equals false
     # Unless if year is divisible by 400, leap equals true
 
+# SUCCESSFUL SOLUTION
 # def leap_year?(year)
 #   leap = false
 #   if year % 4 == 0
 #     leap = true
-#   elsif year % 100 == 0
-#     leap = false
-#     unless
-#       if year % 400 == 0
-#         leap = true
-#       end
-#     end
 #   end
+#   if year % 100 == 0
+#     leap = false
+#   end
+#   if year % 400 == 0
+#     leap = true
+#   end
+#   leap
 # end
 
+# REFACTORED SOLUTION
 def leap_year?(year)
   leap = false
   if year % 4 == 0
     leap = true
   end
-  if year % 100 == 0
+  if year % 100 == 0 && year % 400 != 0
     leap = false
-  end
-  if year % 400 == 0
-    leap = true
   end
   leap
 end
 
+p leap_year?(1933)
 p leap_year?(1920)
+p leap_year?(1600)
 p leap_year?(400)
 p leap_year?(200)
 p leap_year?(6)
+p leap_year?(0) # Edgecase Consideration: Does zero count as divisible by 400 in terms of leap years? I imagine so, and given the instructions do not specify otherwise, it would be logical to assume that between the years -400 and 400, year 0 would be a leap year.
