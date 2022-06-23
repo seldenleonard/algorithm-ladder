1285ad988edb0cfed58c5f1127660f2209a63e84
seldenleonard <selden222@gmail.com>
2022-06-23 03:46:59 -0700

    adds instructions for leap-year.rb

---
+++ basic-numbers-problems/leap-year.rb
@@ -0,0 +1,16 @@
+# INSTRUCTIONS
+# Given a year, report if it is a leap year.
+# The tricky thing here is that a leap year in the Gregorian calendar occurs:
+  # On every year that is evenly divisible by 4
+  # Except every year that is evenly divisible by 100
+  # Unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+# If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+# Clarify
+# Visualize
+# Write the Code
+# Debugger
+# Refactor
+
+# STEPS
