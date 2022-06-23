04a55c3635c35c80488bba65187d691e5da4cfdf
seldenleonard <selden222@gmail.com>
2022-06-23 02:38:39 -0700

    writes out whiteboarding steps for fizzbuzz

--- basic-numbers-problems/fizzbuzz.rb
+++ basic-numbers-problems/fizzbuzz.rb
@@ -1,5 +1,5 @@
 # INSTRUCTIONS
-# Write a function that prints out every number from 1 to N, with the following exceptions:
+# Write a function that prints out every number from 1 to n, with the following exceptions:
   # If the number is divisible by 3, print out "FIZZ".
   # If the number is divisible by 5, print out "BUZZ".
   # If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
@@ -11,3 +11,5 @@
 # Refactor
 
 # STEPS
+# Define a method that accepts an integer
+# Using a 'for' loop, iterate through each number between 1 and n, evaluate for the given criteria.
