846c4b870ccf1d054f374438da93b7bd8df14370
seldenleonard <selden222@gmail.com>
2022-06-23 03:36:54 -0700

    solves fizzbuzz.rb algorithm

--- basic-numbers-problems/fizzbuzz.rb
+++ basic-numbers-problems/fizzbuzz.rb
@@ -13,3 +13,22 @@
 # STEPS
 # Define a method that accepts an integer
 # Using a 'for' loop, iterate through each number between 1 and n, evaluate for the given criteria.
+
+# REFACTORING IDEAS
+# Because every integer is going to be evaluated twice (for divisibility by 3 and 5), if I can calculate every number that is divisible by 3 and/or 5 between 1 and n, I can effectively remove all evaluations and simply print all the numbers that are not and then use a predictive method to print FIZZ, BUZZ, and FIZZBUZZ.
+
+def fizzbuzz(n)
+  (1...n).each do |int|
+    if int % 3 == 0 && int % 5 == 0
+      p "FIZZBUZZ"
+    elsif int % 3 == 0
+      p "FIZZ"
+    elsif int % 5 == 0
+      p "BUZZ"
+    else
+      p int
+    end
+  end
+end
+
+fizzbuzz(33)
