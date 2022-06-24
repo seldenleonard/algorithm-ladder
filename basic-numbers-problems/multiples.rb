c5a8cd8663d520ecf31c28e16a16685e998addc4
seldenleonard <selden222@gmail.com>
2022-06-24 03:26:47 -0700

    solves multiples algorithm but does not account for multiples shared by 3 and 5

--- basic-numbers-problems/multiples.rb
+++ basic-numbers-problems/multiples.rb
@@ -13,12 +13,28 @@
 
 # STEPS
 # Define a method that accepts an integer
-# Define a variable called "sum" and set it equal to zero
 # Create an index variable and set it equal to 1
-# Write a loop that continues to run while index * 3 is less than or equal to the input integer
+# Define a variable called "sum" and set it equal to zero
+# Write a loop that runs while index * 3 is less than the input integer
   # Within this loop, add the result of index * 3 to the "sum" variable
   # Then increase index by 1
-# Reset index equal to 0
+# Reset index equal to 1
 # Write a second loop identical to the previous one, with the only differences being that instead of doing index * 3, it uses index * 5
 # Return sum
 
+def multiples(int)
+  index = 1
+  sum = 0
+  while index * 3 < int
+    sum += index * 3
+    index += 1
+  end
+  index = 1
+  while index * 5 < int
+    sum += index * 5
+    index += 1
+  end
+  sum
+end
+
+p multiples(1000)
