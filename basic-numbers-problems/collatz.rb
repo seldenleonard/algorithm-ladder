228669867cfb68ad709f81f2cc7144df2777a0fd
seldenleonard <selden222@gmail.com>
2022-06-27 18:48:39 -0700

    writes out whiteboarding steps

--- basic-numbers-problems/collatz.rb
+++ basic-numbers-problems/collatz.rb
@@ -1,3 +1,4 @@
+# INSTRUCTIONS
 # The Collatz Conjecture or 3x+1 problem can be summarized as follows:
   # Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
   # Given a number n, return the number of steps required to reach 1.
@@ -21,4 +22,14 @@
 # Debugger
 # Refactor
 
-# INSTRUCTIONS
+# STEPS
+# Define a method that accepts an integer n
+# Set a variable called 'steps' equal to zero
+# While n != 1
+# If n % 2 == 0 (or if n.even? is true)
+  # n = n / 2
+  # steps += 1
+# If n % 2 != 0 (or if n.even? is false)
+  # n = (n * 3) + 1
+  # steps += 1
+# Return steps
