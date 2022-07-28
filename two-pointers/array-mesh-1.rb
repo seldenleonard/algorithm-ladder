714bea04541a557b60637c4ab9517cbc02c26c34
seldenleonard <selden222@gmail.com>
2022-07-28 01:48:07 -0700

    writes out whiteboarding steps to complete array-mesh-1 algorithm

--- two-pointers/array-mesh-1.rb
+++ two-pointers/array-mesh-1.rb
@@ -3,3 +3,12 @@
   # Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
 
 # STEPS
+# Define two arrays of strings
+# Create two indexes
+# Define an empty array
+# Write a loop that iterates through the length of array1
+  # Write a nested loop that iterates through the length of array2
+    # Push the concatenated combination of array1[i] and {array2[i2] into the empty array
+  # When the nested loop concludes, reset the second index to zero so it can go through another looping cycle
+# Return the previously empty array that is now filled with values that combine the values from the two arrays we started with
+
