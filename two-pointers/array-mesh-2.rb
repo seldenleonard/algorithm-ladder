e6984a8976d6e20df006d6d7ce32d5e4b41ab741
seldenleonard <selden222@gmail.com>
2022-07-28 01:54:24 -0700

    adds instructions

---
+++ two-pointers/array-mesh-2.rb
@@ -0,0 +1,3 @@
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
+  # Input: ["a", "b", "c", "d"]
+  # Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
