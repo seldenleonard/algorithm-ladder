1a61a3d93537650214bbf9d13d7ce9e1e206c73a
seldenleonard <selden222@gmail.com>
2022-07-28 01:49:17 -0700

    solves array mesh 1 problem

--- two-pointers/array-mesh-1.rb
+++ two-pointers/array-mesh-1.rb
@@ -10,5 +10,21 @@
   # Write a nested loop that iterates through the length of array2
     # Push the concatenated combination of array1[i] and {array2[i2] into the empty array
   # When the nested loop concludes, reset the second index to zero so it can go through another looping cycle
-# Return the previously empty array that is now filled with values that combine the values from the two arrays we started with
+# Print the previously empty array that is now filled with values that combine the values from the two arrays we started with
 
+array1 = ["a", "b", "c"]
+array2 = ["d", "e", "f", "g"]
+
+i = 0
+i2 = 0
+combined_array = []
+while i < array1.length
+  while i2 < array2.length
+    combined_array << "#{array1[i]}#{array2[i2]}"
+    i2 += 1
+  end
+  i2 = 0
+  i += 1
+end
+
+p combined_array
