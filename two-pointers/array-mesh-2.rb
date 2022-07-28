47a1ed9070c5f2e011cde10096bd9a490b96a734
seldenleonard <selden222@gmail.com>
2022-07-28 01:59:26 -0700

    writes out whiteboarding steps to complete array-mesh-2 problem

--- two-pointers/array-mesh-2.rb
+++ two-pointers/array-mesh-2.rb
@@ -1,3 +1,14 @@
 # Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
   # Input: ["a", "b", "c", "d"]
   # Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+# STEPS
+# Define an array
+# Create two indexes
+# Define an empty array
+# Write a loop that iterates through the length of the array using index1
+  # Write a nested loop that iterates through the length of the array using index2
+    # Write an if statement that says unless index1 is equal to index2, push the concatenated combination of array[index1] and array[index2] into the empty array
+  # When the nested loop concludes, reset index2 to zero so it can go through another looping cycle within its parent loop
+# Return the previously empty array that is now filled with values that combine the values from the two arrays we started with
+
