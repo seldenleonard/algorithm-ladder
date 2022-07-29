1651705ddaf137a9a6df83cd8bcc429665a0ebe7
seldenleonard <selden222@gmail.com>
2022-07-29 00:43:02 -0700

    solves array mesh 2 algorithm except for repeating values when at same index

--- two-pointers/array-mesh-2.rb
+++ two-pointers/array-mesh-2.rb
@@ -3,12 +3,27 @@
   # Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
 
 # STEPS
-# Define an array
+# Define an array containing strings
 # Create two indexes
-# Define an empty array
+# Create an empty array
 # Write a loop that iterates through the length of the array using index1
   # Write a nested loop that iterates through the length of the array using index2
     # Write an if statement that says unless index1 is equal to index2, push the concatenated combination of array[index1] and array[index2] into the empty array
   # When the nested loop concludes, reset index2 to zero so it can go through another looping cycle within its parent loop
 # Return the previously empty array that is now filled with values that combine the values from the two arrays we started with
 
+letters = ["a", "b", "c", "d"]
+letters_combinations = []
+i = 0
+i2 = 0
+
+while i < letters.length
+  while i2 < letters.length
+    letters_combinations << "#{letters[i]}#{letters[i2]}"
+    i2 += 1
+  end
+  i2 = 0
+  i += 1
+end
+
+p letters_combinations
